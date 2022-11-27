88a7aed7dbccef53404849d1bf8561fee5b93d6b
Andrey <6198760@gmail.com>
2022-11-27 18:38:22 +0300

    solve zadacha 13

--- zadacha_13/Program.cs
+++ zadacha_13/Program.cs
@@ -3,3 +3,20 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначиное число ");   //просим ввести число
+int numberIn = int.Parse(Console.ReadLine());       //запоминаем введенное число
+
+if (numberIn < 100) Console.WriteLine("-> третьей цифры нет");             //выводим результат
+else 
+{
+while (numberIn > 999)
+{
+    numberIn = numberIn / 10;
+
+}
+int thirdDigit = numberIn % 10;             //определяем 3 цифру числа
+Console.WriteLine($"-> {thirdDigit}");
+}
