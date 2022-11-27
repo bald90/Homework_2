79e49a432b6841812fa773fd8e056861f273b052
Andrey <6198760@gmail.com>
2022-11-27 19:15:00 +0300

    solve zadacha 15

--- zadacha_15/Program.cs
+++ zadacha_15/Program.cs
@@ -3,3 +3,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.Write("Введите порядковый номер дня недели: "); //вводим цифру
+int numberDay = int.Parse(Console.ReadLine());
+
+string[] weekDays = new string[8];                      //задаем массив
+
+weekDays[1] = "Нет";                                    //обозначаем выходные/рабочие дни
+weekDays[2] = "Нет";
+weekDays[3] = "Нет";
+weekDays[4] = "Нет";
+weekDays[5] = "Нет";
+weekDays[6] = "Да";
+weekDays[7] = "Да";
+
+Console.WriteLine($"{numberDay} -> {weekDays[numberDay]}");
