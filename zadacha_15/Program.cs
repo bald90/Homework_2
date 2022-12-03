fb6d1248f54aaec83679be5956adedc19ca45835
Andrey <6198760@gmail.com>
2022-12-03 16:35:12 +0300

    solve zadacha 15 v2

--- zadacha_15/Program.cs
+++ zadacha_15/Program.cs
@@ -8,14 +8,23 @@ Console.Clear();
 Console.Write("Введите порядковый номер дня недели: "); //вводим цифру
 int numberDay = int.Parse(Console.ReadLine());
 
-string[] weekDays = new string[8];                      //задаем массив
+//вариант № 1
+// string[] weekDays = new string[8];                      //задаем массив
 
-weekDays[1] = "Нет";                                    //обозначаем выходные/рабочие дни
-weekDays[2] = "Нет";
-weekDays[3] = "Нет";
-weekDays[4] = "Нет";
-weekDays[5] = "Нет";
-weekDays[6] = "Да";
-weekDays[7] = "Да";
+// weekDays[1] = "Нет";                                    //обозначаем выходные/рабочие дни
+// weekDays[2] = "Нет";
+// weekDays[3] = "Нет";
+// weekDays[4] = "Нет";
+// weekDays[5] = "Нет";
+// weekDays[6] = "Да";
+// weekDays[7] = "Да";
 
-Console.WriteLine($"{numberDay} -> {weekDays[numberDay]}");
+// Console.WriteLine($"{numberDay} -> {weekDays[numberDay]}");
+
+// вариант № 2
+if (numberDay > 7 && numberDay <1)
+    Console.WriteLine ("введен не корректный номер");
+if (numberDay > 5 && numberDay <8)
+    Console.WriteLine("-> Да");
+if (numberDay> 0 && numberDay <6)
+    Console.WriteLine ("-> Нет");
